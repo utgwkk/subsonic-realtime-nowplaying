3fb349f9d9dca0ba91496d31119b05247571cf37
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-11-21 03:15:09 +0900

    Add close event

--- server/main.py
+++ server/main.py
@@ -25,6 +25,7 @@ def do_stream():
             old_data = new_data
             yield 'data: {}\n\n'.format(json.dumps(new_data))
         time.sleep(1)
+    yield 'event: close\n\n'
 
 @app.route('/stream')
 def streaming():
