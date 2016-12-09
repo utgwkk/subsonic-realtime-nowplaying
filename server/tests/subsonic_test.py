7b216edaf5299344e2ac8f6c12b37c22a950f6f9
UTAGAWA Kiki <utagawakiki@gmail.com>
2016-12-09 10:17:37 +0900

    Add version and app name

--- server/tests/subsonic_test.py
+++ server/tests/subsonic_test.py
@@ -5,9 +5,10 @@ import subsonic
 class SubsonicTest(unittest.TestCase):
     def test_api_build_url(self):
         api = subsonic.SubsonicAPI(user='utgwkk', token='hogeooo', salt='oshio',
-                                   endpoint='http://localhost/subsonic')
+                                   endpoint='http://localhost/subsonic',
+                                   appname='hoge')
         self.assertEqual('http://localhost/subsonic/rest/ping.view'
-                         '?u=utgwkk&t=hogeooo&s=oshio&f=json',
+                         '?u=utgwkk&t=hogeooo&s=oshio&f=json&v=1.14.0&c=hoge',
                          api._build_url('ping'))
 
 if __name__ == '__main__':
